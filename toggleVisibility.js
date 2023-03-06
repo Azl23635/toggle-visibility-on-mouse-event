ebfc68c3e30fb3af191166426526dca07a0ce090
Azl23635 <109496983+Azl23635@users.noreply.github.com>
2023-03-06 13:35:05 +1100

    fix: error catcher

--- toggleVisibility.js
+++ toggleVisibility.js
@@ -1,13 +1,17 @@
 exports.toggleVisibility = function (parent) {
   console.log(parent);
-  if (parent.childElementCount > 0) {
-    let children = parent.childNodes;
-    for (let i = 0; i < children.length; i++) {
-      if (children[i].style.visibility === 'hidden') {
-        children[i].style.visibility = 'visible';
-      } else {
-        children[i].style.visibility = 'hidden';
+  try {
+    if (parent.childElementCount > 0) {
+      let children = parent.childNodes;
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].style.visibility === 'hidden') {
+          children[i].style.visibility = 'visible';
+        } else {
+          children[i].style.visibility = 'hidden';
+        }
       }
     }
+  } catch (e) {
+    console.error(e);
   }
 };
